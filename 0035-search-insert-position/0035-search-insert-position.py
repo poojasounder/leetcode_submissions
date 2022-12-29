e87a39d25850d83553c5f59daa047b198d743174
poojasounder <108160318+poojasounder@users.noreply.github.com>
2022-12-28 23:11:52 -0800

    Time: 33 ms (87.78%), Space: 14.1 MB (55.61%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if target in nums:
+            return nums.index(target)
+        nums.append(target)
+        return sorted(nums).index(target)
+                
+        
